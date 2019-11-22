d02547d8b1ab0e00ddad9a2844e28f2978b3c822
Damian Gerow <dwg@flargle.io>
2019-11-22 03:14:44 -0500

    Simple build script for Alpine on MACCHIATObin

---
+++ README.md
@@ -0,0 +1,93 @@
+### Synopsis
+
+This is a very quick script to build an Alpine distribution for use on the [MACCHIATObin](https://macchiatobin.net/) Single Shot and Double Shot boards.
+
+### Requirements
+
+1. An existing MACCHIATObin board for compiling (cross-compiling has been problematic)
+2. An SD card imaged with the [SolidRun ARMADA A8040 Debian distribution](https://developer.solid-run.com/knowledge-base/armada-8040-debian/)
+3. Confirmed working serial console ([Linux](http://wiki.macchiatobin.net/tiki-index.php?page=Serial+connection+-+Linux) / [Windows](http://wiki.macchiatobin.net/tiki-index.php?page=Serial+connection+-+Windows))
+4. Another computer of some sort. Any sort. Whatever tickles your fancy, but it will need to have a way to write to your MicroSD card
+5. Oh right: a spare MicroSD card. Almost any size will do -- this build is about 500MB in size when completed.
+6. Some spare time
+
+NB: Minicom was acting strangely for me on Linux; I found it much easier to use `cu`: `cu -l /dev/ttyUSB0 -s 115200` (you'll need to have appropriate permissions to open the serial device -- for Debian distributions, add yourself to the `dialout` group)
+
+### Instructions
+
+1. (Optional) [Update the U-Boot](https://developer.solid-run.com/knowledge-base/armada-8040-machiatobin-u-boot-and-atf/) on your board.
+** If you put the resulting flash-image.bin on the root of a FAT32-formatted MicroSD card, you can update u-boot with `bubt flash-image.bin spi mmc` at the u-boot prompt
+2. Get your Debian-based MACCHIATObin online
+3. Check out this source tree, and copy it to the host
+4. SSH over (default username/password is `debian`), spin up a `tmux` session, and type `bash build-image.sh`
+5. Once complete, copy the resulting tarball back to your computer
+6. Initialize your MicroSD card with an MSDOS partition table -- using UEFI can cause U-Boot to behave weirdly
+7. Create a single partition, of at least 550MB, and format it with your filesystem of choice -- confirmed that FAT32 and ext4 both work just fine
+8. Explode the tarball you copied in #5 above into the root of the MicroSD card.
+
+You should now be able to put this MicroSD card into your MACCHIATObin board, power it on, and 30-45 seconds later, you'll have an Alpine prompt on the serial console.
+
+Note that I have *not* tested this with the U-Boot that ships on the boards; I have built something following the instructions above and deployed that; it's considerably newer, and may behave differently than the distributed u-boot. Yes, I'm aware that there are considerably more convoluted instructions pointing you to a Marvell-specific fork of u-boot with some custom patches and checking out weird branches and whatnot -- don't follow those instructions. They work just fine, but so do the ones above (and the ones above seem to be more current).
+
+### TODO
+
+All the things I'd like to change but haven't gotten around to yet:
+
+1. Add more packages in the base distribution. Specifically, I'm looking at adding `nftables` for firewall management, `tcpdump` and `ethtool` to troubleshoot network issues, and ... I forget the rest, but I have them noted down ... somewhere.
+2. Clean up the kernel configuration a bit.
+3. Produce an SD card image, instead of a tarball.
+4. Automatically identify the latest Alpine distribution and use that -- maybe even be able to specify `edge` or `stable`.
+5. Tune up the base install to mount the on-board `mmc` device automatically (to be used for LBU)
+6. Really clean up that build script. It's pretty rough.
+
+### FAQ
+#### Why is there a FAQ? Nobody uses this but you.
+Well, yeah. This is more of a "Fun and Anticipated Questions" than a "Frequently Asked Questions".
+
+#### What's a Fun question?
+Depends on your perspective -- but I find asking people about their grandmothers to be fun.
+
+#### How do I create an MSDOS partition table?
+WARNING: THIS WILL REMOVE ALL YOUR DATA
+
+If you think you might possibly have something stored on that MicroSD card that maybe you, or your grandmother, will want to access at some point in the future, please back it up now. You *will* lose that data during this process.
+
+In Windows:
+1. Insert the MicroSD card into your computer, using whatever mechanism you have: native MicroSD reader, SD reader and a MicroSD adapter, or some new-fangled USB dongle of some sort that maybe does both
+2. Launch Powershell as admin (Search Bar -> "Windows Powershell" -> Right-Click -> "Run as administrator")
+3. Say that "Yes", you want to allow this program to make changes to your computer
+4. Type `diskpart` (followed by the enter button, of course) to launch the disk partition manager
+5. Type `list disk` to show the available disks, and figure out which one is your MicroSD card (hint: it will *not* be Disk 0)
+6. Type `select disk <n>`, where `n` is the number of the disk assigned to your MicroSD card
+7. Type `clean` (this is the step that removes all your data)
+8. Type `convert mbr` to create a new MSDOS partition table
+9. Type `create partition primary` to create a new partition (`diskpart` will also magically select this partition for us, which is one less thing for us to type out!)
+10. Type `format quick fs=fat32 label="ALPINE"` to format this new partition
+11. Sometimes Windows doesn't like to make the drive actually be available (¯\_(ツ)_/¯), so we gotta do that part for us. Pick a drive letter that isn't in use -- in my case, let's use `J` -- and assign it: `assign letter=j`
+
+Voila! You now have a blank, MSDOS-compatible, FAT32-formatted MicroSD card at your chosen drive letter (`J:`, if you followed everything verbatim).
+
+In Linux:
+1. Insert the MicroSD card into your computer. Since you're running Linux, you probably have a homebrew MicroSD reader attached to a breadboard somewhere in the back of a project drawer.
+2. Launch whatever newfangled partition manager all the Linux kids are using these days: `fdisk`? `cfdisk`? `gdisk`? `parted`? `debug`?
+3. Refer to whatever documentation exists to figure out how to create an MSDOS partition table. Create it.
+
+Yeah, okay, this is less than helpful. But it's late, I'm tired, and I don't want to admit to the world that I still use `fdisk` and don't know how to use any of the newer partition managers.
+
+#### I've previously mucked about with u-boot and things are a bit weird now -- help!
+The boards are pretty forgiving, thankfully. If you trash u-boot in the SPI, you can flash it to an MMC card directly (instructions can be found in the `Update the U-Boot` link above), and re-flash to SPI from there. This is a bit too complicate for me to toss as an answer in a light-hearted FAQ, so feel free to hit me up if you need help (open an issue, @-me on Twitter, whatever).
+
+If you think it's just the u-boot environment that needs cleaning, that's an easy fix! Get on your console, power up the board, interrupt the boot process, and then just type `env default -a` followed by `env save`. That's it! Your U-Boot configuration is reset to defaults. At this point, I would recommend removing power from the board -- a simple `reset` doesn't always work as expected here.
+
+#### This kernel configuration looks ... strange.
+Yeah. Well, as I noted above, SD cards are slow. Almost the entirety of the boot time is spent reading the `initramfs` from the SD card, so I'm trying to cut out as many kernel modules as I possible can. The first successful boot I had took almost a full minute, and I managed to cut that down to about 30 seconds just by stripping unnecessary cruft from the kernel (do you really need Android driver support on this thing?).
+
+I still have a fair bit of work to do here: some more modules to cut out, some others to add back in, then finally actually going over the configuration and making sure it actually makes sense. I'm targeting usage as a wired router: no wireless cards, no GPU support, just passin' packets.
+
+#### I'd really like Package X to be in the root
+So would I, probably. Thankfully it's pretty easy to add a package to your system once it's up and running, so modifying the base system is a lower priority right now.
+
+#### Why do we build on Debian?
+Because all my cross-compiling efforts were failing, and using SolidRun's Debian distribution was the easiest way to get a stable system up and running, with a reasonably current version of `gcc` available.
+
+This will *probably* work on any Debian derivative (i.e. Ubuntu) with no modifications, but I haven't tested it. Using this script with any other distribution will require some slight modifications -- probably just to the package installation stuff, and the build flags we use when building the kernel.
---
+++ build-image.sh
@@ -0,0 +1,246 @@
+#!/usr/bin/env bash
+
+set -Eeu
+
+ALPINE_VERSION="3.10"
+ALPINE_PATCHLEVEL="3"
+KERNEL_VERSION="5.4-rc8"
+BUILD_ROOT="${HOME}/alpine-macchiatobin"
+
+# The rest of these shouldn't require tuning
+SD_ROOT="${BUILD_ROOT}/sdcard"
+ALPINE_DISTRO="${BUILD_ROOT}/alpine-uboot"
+INITRAMFS_ROOT="${BUILD_ROOT}/initramfs"
+MODLOOP_ROOT="${BUILD_ROOT}/modloop"
+
+error() {
+  # TODO: something about line numbers -- something something ${BASH_LINENO}
+  # Is there an equivalent for calling function?
+  local retcode="${1}"
+  shift
+
+  echo "--! ${*}"
+
+  exit "${retcode:-255}"
+}
+
+install_requirements() {
+  echo "--> Checking required packages..."
+  local required_packages=("bc" "bison" "build-essential" "cpio" "curl" "flex" "gpgv" "squashfs-tools" "u-boot-tools" "xz-utils")
+  local packages_to_add=()
+
+  for pkg in "${required_packages[@]}"
+  do
+    dpkg-query -W "${pkg}" > /dev/null 2>&1 || packages_to_add+=("${pkg}")
+  done
+
+  if [[ "${#packages_to_add[*]}" -gt 0 ]]
+  then
+    echo "--> Installing required packages..."
+    echo "We need to install a number of packages to build the image: ${packages_to_add[*]}"
+    sudo -p "Enter your password to install these packages:" apt install "${packages_to_add[@]}"
+  fi
+}
+
+get_alpine_distribution() {
+  local keyring="${PWD}/trustedkeys.kbx"
+  test -f "${keyring}" || error 7 "Could not find keyring to validate GPG signatures at '${keyring}'."
+
+  cd "${BUILD_ROOT}" || error 3 "Could not find our build root at ${BUILD_ROOT}."
+
+  # TODO: Identify the latest patchlevel automatically
+  echo "--> Downloading Alpine AARCH64 u-boot distribution..."
+  curl -sLO "http://dl-cdn.alpinelinux.org/alpine/v${ALPINE_VERSION}/releases/aarch64/alpine-uboot-${ALPINE_VERSION}.${ALPINE_PATCHLEVEL}-aarch64.tar.gz" \
+    || error "${?}" "Failed to download Alpine u-boot distribution."
+  curl -sLO "http://dl-cdn.alpinelinux.org/alpine/v${ALPINE_VERSION}/releases/aarch64/alpine-uboot-${ALPINE_VERSION}.${ALPINE_PATCHLEVEL}-aarch64.tar.gz.asc" \
+    || error "${?}" "Failed to download Alpine u-boot distribution signature."
+
+  echo "--> Validating distribution signature..."
+  gpgv --keyring "${keyring}" \
+    "alpine-uboot-${ALPINE_VERSION}.${ALPINE_PATCHLEVEL}-aarch64.tar.gz.asc" \
+    "alpine-uboot-${ALPINE_VERSION}.${ALPINE_PATCHLEVEL}-aarch64.tar.gz" \
+    || error "${?}" "Failed to validate signatures of the Alpine distribution!"
+
+  echo "--> Extracting distribution..."
+  tar -C "${ALPINE_DISTRO}" -xf "alpine-uboot-${ALPINE_VERSION}.${ALPINE_PATCHLEVEL}-aarch64.tar.gz" \
+   || error "${?}" "Failed to extract Alpine u-boot distribution."
+
+  # I don't like relying on `cd -`, but the output of pushd/popd can be confusing
+  cd -
+}
+
+extract_initramfs() {
+  test -d "${INITRAMFS_ROOT}" || error 3 "Could not find our initramfs root at '${INITRAMFS_ROOT}'."
+
+  echo "--> Extracting initramfs from distribution..."
+  zcat "${ALPINE_DISTRO}/boot/initramfs-vanilla" | cpio -D "${INITRAMFS_ROOT}" -idm \
+    || error "${?}" "Failed to extract Alpine initramfs image."
+
+  echo "--> Cleaning kernel modules and firmware images from extracted initramfs..."
+  rm -rf "${INITRAMFS_ROOT}/lib/modules/*" "${INITRAMFS_ROOT}/lib/firmware/*" \
+    || error "${?}" "Failed to clean kernel-specific binaries from initramfs image."
+}
+
+get_kernel() {
+  cd "${BUILD_ROOT}" || error 3 "Could not find our build root at ${BUILD_ROOT}."
+
+  echo "--> Downloading Linux kernel..."
+  curl -sLO "https://git.kernel.org/torvalds/t/linux-${KERNEL_VERSION}.tar.gz" \
+    || error "${?}" "Failed to download Linux kernel ${KERNEL_VERSION}."
+
+  # TODO: monitor for a proper 5.4 release and update once that's published
+  # TODO: figure out what happens when we need to support multiple major/minor versions
+  # Since we're grabbing an RC kernel, there's no signature, and the URL is different.
+  # curl -sLO "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-${KERNEL_VERSION}.tar.xz" \
+  #   || error "${?}" "Failed to download Linux kernel ${KERNEL_VERSION}."
+  # curl -sLO "https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-${KERNEL_VERSION}.tar.sign" \
+  #   || error "${?}" "Failed to download Linux kernel signature for ${KERNEL_VERSION}."
+  #
+  # --> echo "Validating kernel signature..."
+  # xzcat linux-${KERNEL_VERSION}.tar.xz | gpgv linux-${KERNEL_VERSION}.tar.sign -)
+
+  echo "--> Extracting Linux kernel..."
+  tar -xf linux-${KERNEL_VERSION}.tar.gz \
+    || error "${?}" "Failed to extract kernel."
+  # tar -xf linux-${KERNEL_VERSION}.tar.xz \
+  #   || error "${?}" "Failed to extract kernel."
+
+  cd -
+}
+
+build_kernel() {
+  echo "--> Copying in kernel configuration..."
+  cp kernel-config "${BUILD_ROOT}/linux-${KERNEL_VERSION}/.config" \
+    || error "${?}" "Failed to deploy kernel configuration."
+
+  cd "${BUILD_ROOT}/linux-${KERNEL_VERSION}" \
+    || error "${?}" "Failed to chane to Linux kernel source tree."
+
+  echo "--> Updating kernel configuration..."
+  make oldconfig \
+    || error "${?}" "Failed to update kernel configuration."
+
+  echo "--> Building the kernel (this will take a while)..."
+  local numcpus
+  numcpus="$(grep -c -E '^processor' /proc/cpuinfo 2>/dev/null)"
+  test "${numcpus}" -ge 0 || numcpus=0
+
+  CPPFLAGS=$(dpkg-buildflags --get CPPFLAGS) \
+    CFLAGS=$(dpkg-buildflags --get CFLAGS) \
+    CXXFLAGS=$(dpkg-buildflags --get CXXFLAGS) \
+    LDFLAGS=$(dpkg-buildflags --get LDFLAGS) \
+    make -j$((${numcpus} + 1)) Image modules dtbs > "${LOGDIR}/kernel-build.out" 2>&1 \
+    || error "${?}" "Failed to build the kernel; see ${LOGDIR}/kernel-build.out for details."
+
+  cd -
+}
+
+deploy_kernel() {
+  test -d "${INITRAMFS_ROOT}" || error 3 "Could not find our initramfs root at '${INITRAMFS_ROOT}'."
+  test -d "${MODLOOP_ROOT}" || error 17 "Could not find our modloop root at '${MODLOOP_ROOT}'."
+
+  echo "--> Installing modules for initramfs..."
+  INSTALL_MOD_PATH=${INITRAMFS_ROOT} make -C "${BUILD_ROOT}/linux-${KERNEL_VERSION}" modules_install \
+    || error "${?}" "Failed to install kernel modules to initramfs."
+
+  # The modloop root should contain a single directory called `modules`, which is the base of all
+  # kernel modules
+  echo "--> Copying modules for modloop..."
+  cp -R "${INITRAMFS_ROOT}/lib/modules" "${MODLOOP_ROOT}" \
+    || error "${?}" "Failed to copy kernel modules to modloop."
+
+  echo "Copying kernel in place..."
+  install -m 0755 "${BUILD_ROOT}/linux-${KERNEL_VERSION}/arch/arm64/boot/Image" "${SD_ROOT}/boot/vmlinuz-${KERNEL_VERSION}" \
+    || error "${?}" "Failed to deploy kernel image to ${SD_ROOT}/boot."
+
+  echo "Copying kernel config in place..."
+  install -m 0644 "${BUILD_ROOT}/linux-${KERNEL_VERSION}/.config" "${SD_ROOT}/boot/config-${KERNEL_VERSION}" \
+    || error "${?}" "Failed to deploy kernel configuration to ${SD_ROOT}/boot."
+
+  echo "Copying System.map in place..."
+  install -m 0644 "${BUILD_ROOT}/linux-${KERNEL_VERSION}/System.map" "${SD_ROOT}/boot/System.map-${KERNEL_VERSION}" \
+    || error "${?}" "Failed to deploy kernel map to ${SD_ROOT}/boot."
+
+  echo "Installing dtbs..."
+  INSTALL_PATH="${SD_ROOT}/boot" make -C "${BUILD_ROOT}/linux-${KERNEL_VERSION}" dtbs_install
+  
+  # We have to temporarily work around the fact that our kernel version is "5.4-rc8", but the built kernel identifies itself as "5.4.0-rc8"
+  mv "${SD_ROOT}/boot/dtbs/*" "${SD_ROOT}/boot/dtbs/${KERNEL_VERSION}"
+  
+  # Our boot script looks for a specific dtb at `/boot/dtbs/<kernel>/<name>.dtb`, so make that available
+  # We copy instead of move, so we're still lined up with the more traditional dtb placement (/boot/dtbs/<kernel>/<vendor>/<name>.dtb)
+  cp "${SD_ROOT}/boot/dtbs/${KERNEL_VERSION}/marvell/armada-8040-*" "${SD_ROOT}/boot/dtbs/${KERNEL_VERSION}"
+}
+
+create_initramfs() {
+  # We specify `newc` because that's the format the kernel expects
+  # And maximize the compression because sd cards are slow
+  # TODO: can we use a better form of compression here?
+  echo "--> Creating initramfs..."
+  find "${INITRAMFS_ROOT}" -printf '%P' | cpio -H newc -o | gzip -9 > "${BUILD_ROOT}/initramfs-${KERNEL_VERSION}" \
+    || error "${?}" "Failed to create initramfs."
+
+  echo "--> Building u-boot compatible initramfs..."
+  mkimage -n "${BUILD_ROOT}/initramfs-${KERNEL_VERSION}" -A arm -O linux -T ramdisk -C none -d "${BUILD_ROOT}/initramfs-${KERNEL_VERSION}" "${SD_ROOT}/boot/initramfs-${KERNEL_VERSION}" \
+    || error "${?}" "Failed to u-boot-ize our initramfs."
+  
+  rm -f "${BUILD_ROOT}/initramfs-${KERNEL_VERSION}" || true
+}
+
+create_modloop() {
+  # We need to explicitly remove the previous modloop just in case
+  # If one is still there, `mksquashfs` will try to merge rather than overwrite
+  rm -f "${SD_ROOT}/boot/modloop-${KERNEL_VERSION}" \
+    || error "${?}" "Failed to remove modloop from a previous build."
+  
+  # Ratchet up the compression as much as possible to speed up boot times
+  # Again, SD cards are slow
+  echo "--> Creating modloop..."  
+  mksquashfs "${MODLOOP_ROOT}/" "${SD_ROOT}/boot/modloop-${KERNEL_VERSION}" -b 1048576 -comp xz -Xdict-size 100%
+}
+
+create_boot_script() {
+  echo "--> Setting boot script variables..."
+  sed -e "s/@@KERNEL_VERSION@@/${KERNEL_VERSION}/" boot.cmd > "${SD_ROOT}/boot/boot.cmd" \
+    || error "${?}" "Failed to configure boot script with our kernel version."
+
+  echo "--> Building u-boot compatible boot script..."
+  mkimage -C none -A arm -T script -d "${SD_ROOT}/boot/boot.cmd" "${SD_ROOT}/boot/boot.scr" \
+    || error "${?}" "Failed to u-boot-ize our boot script."
+}
+
+deploy_package_repository() {
+  # TODO: we need to add a bunch of packages in here, figuring out dependencies and pulling them in automatically
+  # For now, just use the packages provided by upstream in the distribution, and the rest can be installed the normal way
+  ### This is where we need to spend a bit more time on the magic
+  echo "--> Provisioning local package repository..."
+  cp -R "${ALPINE_DISTRO}/apks" "${SD_ROOT}/"
+}
+
+# Ew
+mv "${BUILD_ROOT}" "${BUILD_ROOT}.$(date +"%Y%m%d%H%M%S")"
+mkdir -p "${BUILD_ROOT}"
+mkdir -p "${SD_ROOT}"
+mkdir -p "${SD_ROOT}/boot"
+mkdir -p "${ALPINE_DISTRO}"
+mkdir -p "${INITRAMFS_ROOT}"
+mkdir -p "${MODLOOP_ROOT}"
+
+# TODO: Better output handling, logging, etc.
+# We should have a `log` function, much like `error`, instead of littering the code with `echo`
+LOGDIR="${PWD}"
+
+install_requirements
+get_alpine_distribution
+extract_initramfs
+get_kernel
+build_kernel
+deploy_kernel
+create_initramfs
+create_modloop
+
+# TODO: we should build an actual SD card image instead of just a tarball
+echo "--> Creating tarball that goes into SD card root..."
+(cd "${SD_ROOT}" && tar -cf "${BUILD_ROOT}/alpine-macchiatobin.tar" -- *)
+
+echo "--> All done! You can find your Alpine distribution for the MACCHIATOBIN at ${BUILD_ROOT}/alpine-macchiatobin.tar!"
